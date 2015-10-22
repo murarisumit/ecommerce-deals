544c808a6d48e3fb99f8ecaf805f0b18e90304d5
Sumitmurari <murarisumit@gmail.com>
2015-10-23 01:02:20 +0530

    added requirements.txt

--- app/deals.py
+++ app/deals.py
@@ -9,23 +9,27 @@ import json
 def hello():
     return "Hello World!"
 
+
 @app.route("/deals")
 def get_deals():
-    deals = []
-    page = requests.get('https://affiliate-program.amazon.in/offers')
-    tree = html.fromstring(page.text)
-    element = tree.get_element_by_id("tab-1")
-    managedContent = element.find_class("managedContent")
+    try:
+        deals = []
+        page = requests.get('https://affiliate-program.amazon.in/offers')
+        tree = html.fromstring(page.text)
+        element = tree.get_element_by_id("tab-1")
+        managedContent = element.find_class("managedContent")
 
-    for content in managedContent:
-        for table in content.xpath('table'):
-            data = {}
-            data['href'] = table.xpath('tr/td/a/@href')[0].replace('YourStoreID', 'bodyf01-21')
-            data['heading'] = table.xpath('tr/td/h2')[0].text
-            data['imgsrc'] = table.xpath('tr/td/a/img/@src')[0]
-            element = table.find_class('rightcell')
-            string = element[0].text_content()
-            data['text'] = string.split('\n')[2]
-            deals.append(data)
-    json_data = json.dumps(deals)
+        for content in managedContent:
+            for table in content.xpath('table'):
+                data = {}
+                data['href'] = table.xpath('tr/td/a/@href')[0].replace('YourStoreID', 'bodyf01-21')
+                data['heading'] = table.xpath('tr/td/h2')[0].text
+                data['imgsrc'] = table.xpath('tr/td/a/img/@src')[0]
+                element = table.find_class('rightcell')
+                string = element[0].text_content()
+                data['text'] = string.split('\n')[2]
+                deals.append(data)
+        json_data = json.dumps(deals)
+    except Exception as e:
+        return None   # Will log it later , for time being it none
     return json_data
